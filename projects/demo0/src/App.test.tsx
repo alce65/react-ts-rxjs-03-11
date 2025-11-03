3d49dbfe71575b02cc1adb02d6a20cf1d9cb253f
Alejandro Cerezo <alce65@hotmail.es>
2025-11-03 16:32:36 +0100

    Add project demo0 with react-ts-vitest

---
+++ projects/demo0/src/App.test.tsx
@@ -0,0 +1,5 @@
+describe('App', () => {
+  test('renders correctly', () => {
+    // Test implementation
+  });
+});
---
+++ projects/demo0/vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['**/*.test.ts', '**/*.test.tsx'],
+    // opcionalmente, para configurar la cobertura
+    coverage: {
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
+      exclude: ['src/main.tsx', 'src/**/types/*.ts'],
+    },
+  }
+})
