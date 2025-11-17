253e7aec847d26f13d239b40125b0cf31c9728de
Alejandro Cerezo <alce65@hotmail.es>
2025-11-17 17:48:28 +0100

    Add get data with fake fetch

---
+++ projects/demo2.3.rx/src/components/get-data/get-data.tsx
@@ -0,0 +1,45 @@
+const fetchMock = (): Promise<string> => {
+    return new Promise((resolve) => {
+        // Simulamos una petición a un API
+        setTimeout(() => {
+            console.log('Fetching data...');
+            resolve('Data from API');
+        }, 1000);
+    });
+};
+
+import React, { useCallback, useRef } from 'react';
+import { Card } from '../core/card/card';
+import { useObservable } from '@/hooks/use-observable.v3';
+import { from, fromEvent, switchMap, tap, type Observable } from 'rxjs';
+
+export const GetData: React.FC = () => {
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    const factory = useCallback((): Observable<string> | null => {
+        if (!buttonRef.current) return null;
+
+        const event$ = fromEvent(buttonRef.current, 'click');
+
+        return event$.pipe(
+            // Cada vez que se hace click, 
+            // //se inicia una nueva petición
+            // Si hay una petición en curso, se cancela
+            switchMap(() =>
+                from(fetchMock()).pipe(
+                    tap((data) => console.log('Data received:', data))
+                )
+            )
+        );
+    }, []);
+
+    const data = useObservable(factory, '');
+
+    return (
+        <Card title="Higher-Order Observable Example">
+            <p>Uso de switchMap para manejar peticiones cancelables</p>
+            <button ref={buttonRef}>Fetch Data</button>
+            {data && <p>Received: {data}</p>}
+        </Card>
+    );
+};
