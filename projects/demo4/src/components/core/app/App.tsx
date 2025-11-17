51dd471ae41da2e4150c0aa92ab13304a81c01b1
Alejandro Cerezo <alce65@hotmail.es>
2025-11-17 18:00:45 +0100

    Add demo 4 clone from demo0

---
+++ projects/demo4/src/components/core/app/App.tsx
@@ -0,0 +1,14 @@
+import { Layout } from '../layout/layout';
+
+
+export const App: React.FC = () => {
+    const title = 'RxJS + API REST';
+
+    return (
+        <Layout appTitle={title}>
+            <main>
+                <p>Demo 4 - Vite + React + TypeScript y API REST</p>
+            </main>
+        </Layout>
+    );
+};
