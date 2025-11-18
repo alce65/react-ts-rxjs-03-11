b9719a2bc12c470fdc9b4f6e7ba1221845386bd3
Alejandro Cerezo <alce65@hotmail.es>
2025-11-18 18:46:16 +0100

    Add fetchService tests

---
+++ projects/demo4/src/services/data-fetch.test.ts
@@ -0,0 +1,72 @@
+import { getData } from './data-fetch';
+
+describe('data-fetch module tests', () => {
+    type Item = {
+        id: number;
+        name: string;
+    };
+
+    const mockData: Item[] = [
+        { id: 1, name: 'Item 1' },
+        { id: 2, name: 'Item 2' },
+    ];
+
+    describe('getData function', () => {
+        const url = 'https://api.example.com/data';
+
+        globalThis.fetch = vi.fn().mockResolvedValueOnce({
+            ok: true,
+            json: async () => [...mockData],
+        } as Response);
+
+        // vi.spyOn(globalThis, 'fetch').mockResolvedValue({
+        //     ok: true,
+        //     json: async () => [
+        //         { id: 1, name: 'Item 1' },
+        //         { id: 2, name: 'Item 2' }
+        //     ]
+        // } as Response);
+
+        test('should get data successfully', () => {
+            const data$ = getData<Item[]>(url)();
+            expect(data$).toBeDefined();
+            data$.subscribe({
+                next: (data) => {
+                    expect(data).toEqual(mockData);
+                    expect(globalThis.fetch).toHaveBeenCalled();
+                },
+                error: (err) => {
+                    console.error('Error occurred:', err);
+                    expect(err).toBeUndefined(); // This should not be called
+                },
+            });
+        });
+
+        test('should handle HTTP ok false', () => {
+            globalThis.fetch = vi.fn().mockResolvedValueOnce({
+                ok: false,
+                status: 404,
+            } as Response);
+            const data$ = getData<Item[]>(url)();
+            expect(data$).toBeDefined();
+            data$.subscribe({
+                error: (err) => {
+                    expect(err.message).toBeDefined();
+                    expect(err.status).toBe(404);
+                }
+            });
+        });
+
+        test('should handle HTTP error', () => {
+            globalThis.fetch = vi.fn().mockRejectedValueOnce(new Error('Network error'));
+            const data$ = getData<Item[]>(url)();
+            expect(data$).toBeDefined();
+            data$.subscribe({
+                error: (err) => {
+                    expect(err.message).toBe('A network error occurred');
+                    expect(err.status).toBe(0);
+                }
+            });
+        });
+    });
+});
